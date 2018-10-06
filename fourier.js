c6cddb2fd84da5d3c4683ae50abadddc55315762
Hayden Blaise <haydenlueck@gmail.com>
2018-10-06 17:02:59 -0600

    Initial commit

---
+++ fourier.js
@@ -0,0 +1,48 @@
+const inputData = [0.0, 0.707, 1, 0.707, 0, -0.707, -1, -0.707];
+
+const result = dft(inputData);
+console.log(result);
+
+function dft(data) {
+    const frequencies = [];
+    const N = data.length;
+    // Can't measure frequencies above Nyquist Limit, N / 2
+    const nyquistLimit = Math.floor(N/2);
+
+    for (let i = 0; i < nyquistLimit; i++) {
+        const sample = data[i];
+        const frequency = calcFreqBin(data, sample, N, i);
+
+        // Double frequency since we only calculated for half the samples
+        const nyquistModifiedFrequency = math.multiply(frequency, 2);
+        // Get magnitude of frequency amplitude via pythagorean theorem
+        const magnitude = math.sqrt(math.add(math.square(nyquistModifiedFrequency.re), math.square(nyquistModifiedFrequency.im)));
+        // Average by dividing by number of samples
+        const amplitude = math.divide(magnitude, N);
+
+        frequencies.push(amplitude);
+    }
+
+    return frequencies;
+}
+
+function calcFreqBin(data, sample, N, k) {
+    let sum = math.complex(0, 0);
+    data.forEach((xSubN, index) => {
+        const innerArg = -k * index * 2*Math.PI / N;
+        const cosElement = Math.cos(innerArg);
+        const sinElement = Math.sin(innerArg);
+        const complex = math.complex(cosElement, sinElement);
+          
+        const final = math.multiply(xSubN, complex);
+        sum = math.add(sum, final);
+    });
+    
+    sum.re = round(sum.re);
+    sum.im = round(sum.im);
+    return sum;
+}
+
+function round(num) {
+    return Math.round(num * 100) / 100
+}
